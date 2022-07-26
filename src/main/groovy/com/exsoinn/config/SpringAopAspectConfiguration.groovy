eed984573342ec4b2e435f287921427c1d1f4f2d
Jose Quijada <joquijada2010@gmail.com>
2022-07-26 15:53:40 -0400

    feat(aop): spring aop fun

---
+++ src/main/groovy/com/exsoinn/config/SpringAopAspectConfiguration.groovy
@@ -0,0 +1,62 @@
+package com.exsoinn.config
+
+import groovy.util.logging.Slf4j
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+
+@EnableAspectJAutoProxy
+@Aspect
+@Configuration
+@Slf4j
+class SpringAopAspectConfiguration {
+
+  /**
+   * Pointcut declarations
+   */
+  @Pointcut("execution(* com.exsoinn.service.MyAwesomeService.*(..))")
+  void interceptAnyMethodInMyAwesomeServiceInterface() {
+
+  }
+
+  /**
+   * Advice declarations
+   */
+  @Around("com.exsoinn.config.SpringAopAspectConfiguration.interceptAnyMethodInMyAwesomeServiceInterface()")
+  Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+    log.info "${'*' * 10} BEGIN ADVICE: aroundAdvice ${'*' * 10}"
+    Object retVal = proceedingJoinPoint.proceed()
+    logJoinPoint(proceedingJoinPoint, ['Return Value': retVal.toString()])
+    log.info "${'*' * 10} END ADVICE: aroundAdvice ${'*' * 10}"
+    return retVal
+  }
+
+  @Around("com.exsoinn.config.SpringAopAspectConfiguration.interceptAnyMethodInMyAwesomeServiceInterface()")
+  Object aroundAdviceNoOp(ProceedingJoinPoint proceedingJoinPoint) {
+    log.info "${'*' * 10} BEGIN ADVICE: aroundAdviceNoOp ${'*' * 10}"
+    log.info "${'*' * 10} DOING A NOOP!!!! ${'*' * 10}"
+    Object retVal = null
+    logJoinPoint(proceedingJoinPoint, ['Return Value': retVal.toString()])
+    log.info "${'*' * 10} END ADVICE: aroundAdviceNoOp ${'*' * 10}"
+    return retVal
+  }
+
+  private void logJoinPoint(JoinPoint joinPoint, Map<String, String> moreStuff = [:]) {
+    joinPoint.with {
+      log.info("""\
+                  Args: $args
+                  Target: $target.class.name
+                  Proxy: ${getThis().class.name}
+                  Method: $signature
+                  """.stripIndent())
+      log.info moreStuff.collect { String key, String value ->
+        "$key: $value"
+      }.join("\n")
+    }
+  }
+
+}
---
+++ src/main/groovy/com/exsoinn/config/SpringAopPracticeConfiguration.groovy
@@ -0,0 +1,15 @@
+package com.exsoinn.config
+
+import com.exsoinn.service.MyAwesomeService
+import com.exsoinn.service.MyAwesomeServiceImpl
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SpringAopPracticeConfiguration {
+
+  @Bean
+  MyAwesomeService myAwesomeService() {
+    new MyAwesomeServiceImpl()
+  }
+}
