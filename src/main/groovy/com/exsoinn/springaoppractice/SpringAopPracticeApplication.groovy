cabf7ad17c64781a7498f168be220d331df25049
Jose Quijada <joquijada2010@gmail.com>
2022-07-26 15:53:40 -0400

    feat(aop): init

---
+++ src/main/groovy/com/exsoinn/springaoppractice/SpringAopPracticeApplication.groovy
@@ -0,0 +1,13 @@
+package com.exsoinn.springaoppractice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringAopPracticeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringAopPracticeApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/exsoinn/springaoppractice/SpringAopPracticeApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.exsoinn.springaoppractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAopPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
